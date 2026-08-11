7da3ad79534371330ec8384dda0d36379d0ae287
artyomalvarez <alvarezmanjarrezjuanjose@gmail.com>
2026-08-10 20:36:29 -0500

    feat : lista empledos

--- SistemaDeEmpleados2/Models/Empresa.cs
+++ SistemaDeEmpleados2/Models/Empresa.cs
@@ -6,6 +6,12 @@ public class Empresa
     public string Nombre { get; set; }
     public string Direccion { get; set; }
     public List<Empleado> ListaEmpleados { get; set; }
+
+    public  Empresa()
+    {
+        ListaEmpleados = new List<Empleado>();
+    }
+    
     
     
     

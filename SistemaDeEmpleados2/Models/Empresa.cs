9e69d2e2970635dd9c2641b495f77c44d71f2bb5
artyomalvarez <alvarezmanjarrezjuanjose@gmail.com>
2026-08-11 00:34:48 -0500

    feat: crud y la interfaz de usuario

--- SistemaDeEmpleados2/Models/Empresa.cs
+++ SistemaDeEmpleados2/Models/Empresa.cs
@@ -1,5 +1,5 @@
 namespace SistemaDeEmpleados2.Models;
-
+using System.Linq;
 public class Empresa
 {
     
@@ -11,8 +11,66 @@ public class Empresa
     {
         ListaEmpleados = new List<Empleado>();
     }
+
+    public void AgregarEmpleado(Empleado empleado)
+    {
+        ListaEmpleados.Add(empleado);
+    }
     
-    
+    public void MostrarTodosLosEmpleados()
+    {
+        foreach (var empleados in ListaEmpleados)
+        {
+            empleados.MostrarInformacion();
+        }
+    }
+
+    public Empleado? BuscarEmpleado(string numeroDeIdentificacion)
+    {
+        return 
+            ListaEmpleados.Where(empleado =>
+            empleado.NumeroDeIdentificacion == numeroDeIdentificacion).FirstOrDefault();
+    }
+    public void EliminarEmpleado(string nombre,  string apellido)
+    {
+        var empleadoEncontrado = ListaEmpleados
+        .Where(empleado => 
+            nombre == empleado.Nombre && apellido ==
+            empleado.Apellido)
+        .FirstOrDefault();
+        if (empleadoEncontrado != null)
+        {
+            ListaEmpleados.Remove(empleadoEncontrado);
+        }
+        
+
+    }
+
+    public void ActualizarEmpleado(string numeroDeIdentificacion,
+        string nombre,
+        string apellido,
+        byte edad,
+        string posicion,
+        double salario)
+    {
+        var empleadoEncontrado = BuscarEmpleado(numeroDeIdentificacion);
+        if (empleadoEncontrado != null)
+        {
+            empleadoEncontrado.Nombre = nombre;
+            empleadoEncontrado.Apellido = apellido;
+            empleadoEncontrado.Edad = edad;
+            empleadoEncontrado.Salario = salario;
+            empleadoEncontrado.Posicion = posicion;
+        }
+    }
+
+    public List<Empleado> MostrarEmpleadosPorCargo(string posicion)
+    {
+        return ListaEmpleados
+            .Where(empleado => empleado.Posicion == posicion)
+            .ToList();
+    }
+    }
     
     
 }
--- SistemaDeEmpleados2/Program.cs
+++ SistemaDeEmpleados2/Program.cs
@@ -1,14 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-using SistemaDeEmpleados2.Models;
-Console.WriteLine("Hello, World!");
+﻿// using SistemaDeEmpleados2.Models;
+//
+// Empleado empleado = new Empleado
+// {
+//     Id = Guid.NewGuid(),
+//     Nombre = "juan",
+//     Apellido = "Alvarez",
+//     NumeroDeIdentificacion = "1001010",
+//     Edad = 25,
+//     Posicion = "desarrollador",
+//     Salario = 3000000
+// };
+//
+// Empresa empresa = new Empresa();
+//
+// empresa.AgregarEmpleado(empleado);
 
-Empleado empleado = new Empleado
+bool continuar = true;
+Empresa empresa = new Empresa();
+
+while (continuar)
 {
-    Id =  Guid.NewGuid(),
-    Nombre = "juan",
-    Apellido = "Alvarez",
-    NumeroDeIdentificacion = "1001010",
-    Edad = 25,
-    Posicion = "desarrollador",
-    Salario = 3000000
-};
+    Console.Clear();
+    Console.WriteLine("--- SISTEMA DE EMPLEADOS ---");
+    Console.WriteLine("1. Agregar Empleado");
+    Console.WriteLine("2. Salir");
+    Console.Write("Seleccione una opción: ");
+
+    string opcion = Console.ReadLine();
+
+    switch (opcion)
+    {
+        case "1":
+            Console.WriteLine("Ingrese la edad:");
+            string entradaEdad = Console.ReadLine();
+            byte edad;
+
+            if (byte.TryParse(entradaEdad, out edad))
+            {
+                Console.WriteLine("Edad registrada correctamente. Presione ENTER.");
+            }
+            else
+            {
+                Console.WriteLine("Error: La edad debe ser un número entero. Presione ENTER.");
+            }
+            Console.ReadLine();
+            break;
+
+        case "2":
+            continuar = false;
+            break;
+
+        default:
+            Console.WriteLine("Opción no válida. Presione ENTER.");
+            Console.ReadLine();
+            break;
+    }
+}
